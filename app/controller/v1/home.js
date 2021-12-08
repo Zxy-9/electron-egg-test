f090ab6996f0b43578d3c2db1d20f52a5f3f6fdf
晓芸 曾 <810269346@qq.com>
2021-12-08 12:27:57 +0800

    update

---
+++ app/controller/v1/home.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const BaseController = require('../base');
+
+class HomeController extends BaseController {
+
+  async index() {
+    const { ctx } = this;
+
+    const data = {
+      title: 'hello electron-egg'
+    };
+
+    await ctx.render('index.ejs', data);
+  }
+  
+ 
+  
+  async hello() {
+    const { ctx, service } = this;
+
+    const data = {
+      title: 'hello'
+    };
+
+    this.sendSuccess(data);
+  }
+
+  async helloPage() {
+    const { ctx } = this;
+
+    const data = {
+      title: 'hello'
+    };
+
+    await ctx.render('hello.ejs', data);
+  }
+}
+
+
+module.exports = HomeController;
